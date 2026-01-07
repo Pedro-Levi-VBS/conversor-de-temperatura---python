e9721b02997fc9059023884fc469d84b471a98e8
Pedro-Levi-VBS <pedrolevicosta78@gmail.com>
2026-01-07 11:17:23 -0300

    Create conversor de temperatura.py
    um conversor de temperatura simples feito em python treinando o uso de while try if float input, sou um programador iniciante, se você modificar esse código e danificar sua máquina, eu não sou responsável por nenhum dano.

---
+++ conversor de temperatura.py
@@ -0,0 +1,25 @@
+while True:
+    print("🌡️ conversor de temperatura 🌡️")
+    print("Digite 'sair' para encerrar")
+
+    entradaC = input("Digite a temperatura °C e converta para °F: ")
+    if entradaC.lower() == "sair":
+        print("Encerrado")
+        break
+
+    entradaF = input("Digite a temperatura °F e converta para °C: ")
+    if entradaF.lower() == "sair":
+        print("Encerrado")
+        break
+
+    try:
+        temperaturaC = float(entradaC)
+        temperaturaF = float(entradaF)
+    except ValueError:
+        print("Letras não são válidas, digite apenas números!")
+        continue
+
+    conversorC = (temperaturaC * 9/5) + 32
+    conversorF = (temperaturaF - 32) * 5/9
+    print(f"{temperaturaC}°C = {conversorC}°F")
+    print(f"{temperaturaF}°F = {conversorF}°C")
